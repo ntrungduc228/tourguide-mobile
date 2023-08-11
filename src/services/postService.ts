e8738e1631bdee20561d5cb0d5891d86eb181cca
Vi Nguyen <khanhvi2941@gmail.com>
2023-08-11 09:40:48 +0700

    create api for post

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -5,6 +5,7 @@ const url = '/posts';
 const postApi = {
   getPost: () => axiosClientPrivate.get(`${url}`),
   updatePost: (id: number) => axiosClientPrivate.patch(`${url}/${id}`),
+  createPost: () => axiosClientPrivate.post(`${url}`),
 };
 
 export default postApi;
