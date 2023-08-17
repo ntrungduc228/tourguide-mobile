8b8402925c2483f9e0322bcea6c9ffefa07c0758
Vi Nguyen <khanhvi2941@gmail.com>
2023-08-18 03:43:54 +0700

    profile

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -4,6 +4,10 @@ import * as ImagePicker from 'react-native-image-picker';
 import {Button} from 'react-native-paper';
 import storage from '@react-native-firebase/storage';
 import {uploadImage} from '../utils/uploadImage';
+import {useSelector} from 'react-redux';
+import {IRootState} from '../stores';
+import {useNavigation} from '@react-navigation/native';
+import routesScreen from '../navigations/routes';
 interface Action {
   title: string;
   type: 'capture' | 'library';
@@ -47,10 +51,10 @@ export const ProfileScreen = ({}: Props) => {
   };
 
   return (
-    <View className="h-full aligns-center justify-center">
-      <Text className="font-bold text-2xl">Profile</Text>
+    <View className="h-full pt-4 px-2">
+      <Text className="font-bold text-2xl mb-5">Profile</Text>
       <AvatarUpload uriAvatar={uriAvatar} handlePickImage={handlePickImage} />
-      <Button
+      {/* <Button
         onPress={() => {
           console.log('uriAvatar', uriAvatar);
           if (uriAvatar) {
@@ -58,7 +62,7 @@ export const ProfileScreen = ({}: Props) => {
           }
         }}>
         Upload
-      </Button>
+      </Button> */}
     </View>
   );
 };
@@ -72,21 +76,52 @@ const AvatarUpload = ({
   uriAvatar: string;
   handlePickImage: () => void;
 }) => {
+  const profile = useSelector((state: IRootState) => state?.user?.data?.info);
+  const navigation = useNavigation();
   return (
-    <View className="bg-white flex-row m-4 items-center">
-      <Image
-        source={{
-          uri: uriAvatar,
-        }}
-        className="h-[60px] w-[60px] rounded-full"
-      />
-      <Button
-        mode="text"
-        textColor="#4caf50"
-        className="ml-3"
-        onPress={handlePickImage}>
-        Choose an image
-      </Button>
+    <View className="px-4 py-2 ">
+      <View className="flex flex-row items-center">
+        <View>
+          <Image
+            source={{
+              uri:
+                uriAvatar ||
+                'https://as2.ftcdn.net/v2/jpg/03/31/69/91/1000_F_331699188_lRpvqxO5QRtwOM05gR50ImaaJgBx68vi.jpg',
+            }}
+            className="h-[60] w-[60] rounded-full"
+          />
+        </View>
+        <View className="ml-4 flex-1">
+          <Text className="text-black font-bold text-lg">
+            {profile?.fullName}
+          </Text>
+        </View>
+      </View>
+      <View className="mt-6">
+        <Button
+          className="rounded-full mx-0 text-white bg-black"
+          mode="outlined"
+          onPress={() => navigation.navigate(routesScreen.ProfileEdit)}
+          textColor="#fff">
+          Edit your profile
+        </Button>
+      </View>
     </View>
+
+    // <View className="bg-white flex-row m-4 items-center">
+    //   <Image
+    //     source={{
+    //       uri: uriAvatar,
+    //     }}
+    //     className="h-[60px] w-[60px] rounded-full"
+    //   />
+    //   <Button
+    //     mode="text"
+    //     textColor="#4caf50"
+    //     className="ml-3"
+    //     onPress={handlePickImage}>
+    //     Choose an image
+    //   </Button>
+    // </View>
   );
 };
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,10 +1,12 @@
 import {axiosClientPrivate} from '../configs/axios';
+import {User} from '../types/user';
 
 const url = '/users';
 
 const userService = {
   getUserByPhone: (phone: string) =>
     axiosClientPrivate.get(`${url}/search/phone?q=${phone}`),
+  updateProfile: (user: User) => axiosClientPrivate.patch(url, user),
 };
 
 export default userService;
