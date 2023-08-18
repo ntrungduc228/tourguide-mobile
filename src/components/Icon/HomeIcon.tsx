60c1f7672469e2c5d1b353345b310e1c2a1a3a51
Duc Nguyen <ntrungduc228@gmail.com>
2023-08-18 07:03:49 +0700

    update

---
+++ src/components/Icon/HomeIcon.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import Entypo from 'react-native-vector-icons/Entypo';
+
+type Props = {};
+
+export const HomeIcon = (props: Props) => {
+  return <Entypo name="home" />;
+};
+
+export default HomeIcon;
